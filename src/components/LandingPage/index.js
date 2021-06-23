595d1db8d7f482bfecf78964dbdc079f90e7c18d
Anthony Guerrero <adguerrero1935@gmail.com>
2021-06-23 18:05:40 -0400

    Finishing up my LandingPage

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,11 +1,14 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import "./style.css";
+// import NotePad from "../../../public/Images/NotePad-Icon.png";
 
 const landingPage = () => {
     return (
         <div className="container">
-            <div>
-                <button>Add a note 📓</button>
+            <div className="addNoteBtnContainer">
+                 <a className="addNoteBtn">Add a note <img className="notePadIcon" src={process.env.PUBLIC_URL + "./Images/NotePad-Icon.png"} /> </a>
             </div>
         </div>
     )
