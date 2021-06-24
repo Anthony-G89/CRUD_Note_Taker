0897bea22bf6cc0a811ad5fcb0a125c9189407b9
Anthony Guerrero <adguerrero1935@gmail.com>
2021-06-24 19:45:29 -0400

    Almost done working on My Notes section. Also added bootstrap

--- src/components/Notes/index.js
+++ src/components/Notes/index.js
@@ -6,31 +6,45 @@ import { Link } from "react-router-dom";
 function Notes() {
     return (
 
+
         <div className="noteContainer">
             <Link to="/"><a className="backBtn">Back</a></Link>
 
             <h1 className="noteTitle">New Note</h1>
-            <hr />
-            <div className="row">
-                <div className="col-12 ">
-                    <form>
 
-                        <label for="input_text">Note Title:</label>
-                        <input placeholder="Title" id="input_text" type="text" data-length="20" />
-                            <br/>
-                            <br/>
-                        <label for="input_text">Note</label>
-                        <textarea placeholder="Your note.." rows="10" cols="50" data-length="120"></textarea>
-
+            <div className="row newNote">
+                <div className="col-6 ">
+                    <form className="noteFormContainer">
+                        <label htmlFor="input_text" className="titleLabel">Title:</label>
+                        <input id="input_text" type="text" data-length="20" />
+                        <br />
+                        <br />
+                        <label htmlFor="input_text" className="textLabel"> Note Text</label>
+                        <textarea id="body_text" className="noteBody"  type="text" rows="10" cols="50" data-length="120"></textarea>
                     </form>
+                </div>
+            </div>
+
+            <div className="row listOfNotes">
+                <div className="col-lg-12">
+
+                    <h1 className="myNotesTitle">My Notes</h1>
+
+                    <ul className=" list-group-flush liContainer">
+                        <li className="list-group-item">Sports</li>
+                        <li className="list-group-item">History</li>
+                        <li className="list-group-item">Grocery</li>
+                        <li></li>
+                    </ul>
+
                 </div>
 
             </div>
 
 
-
-
         </div>
+
+
     )
 }
 
