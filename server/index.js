fd9c8a05f562222ee33e08c1783ddf2ebf6f3823
Anthony Guerrero <adguerrero1935@gmail.com>
2021-08-21 14:47:26 -0400

    removed process code

--- server/index.js
+++ server/index.js
@@ -1,25 +1,18 @@
 const path = require("path");
-
 require('dotenv').config({ path: path.resolve(process.cwd(), `../.env`) });
 
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-
 const routes = require("../controllers/noteController.js");
 app.use(routes);
-
-
-
-
 
 app.listen(PORT, () => {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
+
