14e031bdcef1d30c33e7e2bc9561e76ac84c3c44
Anthony Guerrero <adguerrero1935@gmail.com>
2021-08-21 15:15:13 -0400

    fixed code in index.js server folder

--- server/index.js
+++ server/index.js
@@ -14,10 +14,10 @@ app.use(routes);
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('build'));
+    app.use(express.static('../build'));
   
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
     });
   }
 
