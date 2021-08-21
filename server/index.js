45dca7a668766a6a9f0f8e7ac138b5cb9d26975d
Anthony Guerrero <adguerrero1935@gmail.com>
2021-08-21 15:08:55 -0400

    added express static for build directory

--- server/index.js
+++ server/index.js
@@ -12,6 +12,16 @@ app.use(express.json());
 const routes = require("../controllers/noteController.js");
 app.use(routes);
 
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    });
+  }
+
+
 app.listen(PORT, () => {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
