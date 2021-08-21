81883310d9e5bb46b4e91174b2901dc7af01fb34
Anthony Guerrero <adguerrero1935@gmail.com>
2021-08-21 15:30:57 -0400

    fixed build route and index.js

--- server/index.js
+++ server/index.js
@@ -14,12 +14,12 @@ app.use(routes);
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('../build'));
-  
+    app.use(express.static(path.resolve(process.cwd(), `../build`)));
+
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+        res.sendFile(path.resolve(process.cwd(), '../build', 'index.html'));
     });
-  }
+}
 
 
 app.listen(PORT, () => {
